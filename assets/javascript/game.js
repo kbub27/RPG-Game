7881fe341c114e3466c146c1b056c846549b47d6
kbub27 <kalebmerriman27@gmail.com>
2019-03-16 11:41:55 -0700

    added images and tags

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,10 @@
 $(document).ready(function () {
     
-
+    var userChar1 = $("<img>");
+    var userChar2 = $("<img>");
+    var userChar3 = $("<img>");
+    var userChar4 = $("<img>");
+    var userChar5 = $("<img>");
+    var userChar6 = $("<img>");
 
 })
